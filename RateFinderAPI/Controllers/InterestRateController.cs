77e456faa0a63f2ec050cd88a3858964e29828bf
tsouza4asset <tsouza@4asset.net.br>
2021-08-22 13:43:14 -0300

    Criando estrutura da solução juntamente com a API InterestRate;

---
+++ RateFinderAPI/Controllers/InterestRateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateFinderAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InterestRateController : Controller
+    {
+        [HttpGet]
+        [Route("/taxaJuros")]
+        public decimal Get()
+        {
+            return new decimal(0.01);
+        }
+    }
+}
---
+++ RateFinderAPITest/Controllers/InterestRateControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using RateFinderAPI.Controllers;
+using Xunit;
+
+namespace RateFinderAPITest.Controllers
+{
+    public class InterestRateControllerTest
+    {
+        InterestRateController _interestRateController;
+
+        public InterestRateControllerTest()
+        {
+            _interestRateController = new InterestRateController();
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsRate001()
+        {
+            // Act
+            var resultRate = _interestRateController.Get();
+
+            // Assert
+            Assert.Equal(new decimal(0.01), resultRate);
+        }
+    }
+}
